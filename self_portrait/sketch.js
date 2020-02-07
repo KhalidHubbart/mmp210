7d479c7e3d7a5c472cbdce137bc7e8f80298044d
KhalidHubbart <60409037+KhalidHubbart@users.noreply.github.com>
2020-02-06 17:39:33 -0800

    Update sketch.js

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -3,10 +3,42 @@ function setup() {
 }
 
 function draw() {
-	background("lightgreen");
+	background(255, 204, 0);
 
-	ellipse(200, 180, 100);
+    // shapes with xy coordinate + size
 
-	ellipse(220, 160, 20);
-	ellipse(160, 160, 30, 10);
+    fill('blue');
+    stroke(0);
+    strokeWeight(3);
+     
+    // face area
+	ellipse(200, 180, 200); // face
+
+	fill('0, 81, 82, 127');
+	stroke(0, 81, 82);
+	circle(220, 160, 30); // right eye
+	circle(160, 160, 30, 10); // left eye
+
+    // mouth
+	stroke('Black');
+	strokeWeight(2);
+	fill('white');
+	arc(200, 230, 80, 80, 0, PI + QUARTER_PI, CHORD);
+
+    // right ear
+    fill('blue');
+	arc(290, 180, 40, 60, -HALF_PI, HALF_PI);
+	arc(290, 180, 30, 50, -HALF_PI, HALF_PI);
+
+    // shapes that are only xy points
+
+    // Hat
+    fill('black');
+    arc(160, 80, 80, 80, PI * 0.75, PI * 0.1, OPEN);
+    
+
+    // nose
+    fill('DARKTURQUOISE');
+    noStroke();
+    triangle(200, 180, 180, 200, 220, 190);
 }
