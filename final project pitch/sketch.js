6c6e1eed180a63fb9a7b310d5635e0b7a91c24b9
KhalidHubbart <60409037+KhalidHubbart@users.noreply.github.com>
2020-05-12 16:12:46 -0400

    update final

--- final project pitch/sketch.js
+++ final project pitch/sketch.js
@@ -1,13 +1,11 @@
 var currentSlide = 0;
 var numberOfSlides = 3;
 
-// next slide button
 var nextButtonX = 850;
 var nextButtonY = 680;
 var nextButtonW = 100;
 var nextButtonH = 33;
 
-// prev slide button
 var prevButtonX = 10;
 var prevButtonY = nextButtonY;
 var prevButtonW = nextButtonW + 30;
@@ -20,13 +18,9 @@ function setup() {
 function draw() {
     background(51);
 
-    // text('slide ' + currentSlide, 100, 100);
-
-    var title; // the title of the slide
+    var title; 
     var description;
     
-
-    // setup each slide
     if (currentSlide == 0) {
         title = "Aviod the circles";
         description = "A interactive game on the page that plays music that is pick for it and the user choose.";
@@ -43,19 +37,15 @@ function draw() {
        square(200, 450, 150); 
     }
 
-    // display title
     textSize(80);
     fill(255);
     noStroke();
     textAlign(LEFT, TOP);
     text(title, 20, 20);
 
-    // display description
     textSize(40);
     text(description, 20, 200, width - 40);
 
-    // slide button display
-    // button background design
     fill('blue');
     stroke(255);
     strokeWeight(2);
@@ -68,8 +58,6 @@ function draw() {
         rect(prevButtonX, prevButtonY, prevButtonW, prevButtonH, 5); // prev
     }
     
-    
-    // text design
     fill(255);
     textSize(25);
     noStroke();
@@ -86,7 +74,6 @@ function draw() {
 }
 
 function mousePressed() {
-    // detecting mouse collision with the next button
     if (mouseX > nextButtonX && mouseX < nextButtonX + nextButtonW &&
         mouseY > nextButtonY && mouseY < nextButtonY + nextButtonH) {
         if (currentSlide < numberOfSlides - 1) {
@@ -95,8 +82,7 @@ function mousePressed() {
         
     }
 
-    // detect mouse collision with prev button
-    if (mouseX > prevButtonX && mouseX < prevButtonX + prevButtonW &&
+     if (mouseX > prevButtonX && mouseX < prevButtonX + prevButtonW &&
         mouseY > prevButtonY && mouseY < prevButtonY + prevButtonH) {
         if (currentSlide > 0) {
             currentSlide--;
